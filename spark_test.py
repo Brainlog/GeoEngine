8b8a9c4455180ee91363dd6ae2719783e702a9e7
Prabal Singh <prabal-singh@outlook.com>
2023-11-06 11:42:09 +0530

    few changes

--- spark_test.py
+++ spark_test.py
@@ -9,17 +9,12 @@ from PIL import Image
 
 from pyspark.sql.types import StructType, StructField, ArrayType, IntegerType
 
-SparkSession.builder.appName("dummy_workload").master("spark://TotallyNormalPC:7077").getOrCreate()
-
 
 class ImageBatch:
     def __init__(self, master_port, file_dir):
         # initialize a SparkSession
-        # grant as 4 executors, and 4g memory each
-        self.spark = SparkSession.builder \
+        self.spark = SparkSession.builder\
         .appName("dummy_workload") \
-        .config("spark_executor_cores", 4) \
-        .config("spark_executor_memory", "4g") \
         .master(master_port) \
         .getOrCreate()
         df = self.spark.read.format("image").option("dropInvalid", True).load(file_dir)
@@ -58,44 +53,46 @@ class ImageBatch:
     
     def __len__(self):
         return self.df.count()
+    
+    def save(self, file_path, format="parquet", **options):
+        self.df.write.save(file_path, format=format, **options)
 
 # Following is sample usage of ImageBatch
 
 file_dir = "Dataset"
 
-# def convert_bgr_array_to_rgb_array(img_array):
-#     B, G, R = img_array.T
-#     return np.array((R, G, B)).T
+def convert_bgr_array_to_rgb_array(img_array):
+    B, G, R = img_array.T
+    return np.array((R, G, B)).T
 
-# def resize_img(img_data, resize=True):
-#     mode = 'RGBA' if (img_data.nChannels == 4) else 'RGB' 
-#     img = Image.frombytes(mode=mode, data=img_data.data, size=[img_data.width, img_data.height])
-#     img = img.convert('RGB') if (mode == 'RGBA') else img
-#     img = img.resize([224, 224], resample=Image.Resampling.BICUBIC) if (resize) else img
-#     # turn the first pixel's rgb values to 1
-#     img.putpixel((0, 0), (1, 1, 1))
-#     arr = convert_bgr_array_to_rgb_array(np.asarray(img))
-#     arr = arr.reshape([224*224*3]) if (resize) else arr.reshape([img_data.width*img_data.height*3])
-#     return arr
+def resize_img(img_data, resize=True):
+    mode = 'RGBA' if (img_data.nChannels == 4) else 'RGB' 
+    img = Image.frombytes(mode=mode, data=img_data.data, size=[img_data.width, img_data.height])
+    img = img.convert('RGB') if (mode == 'RGBA') else img
+    img = img.resize([224, 224], resample=Image.Resampling.BICUBIC) if (resize) else img
+    # turn the first pixel's rgb values to 1
+    img.putpixel((0, 0), (1, 1, 1))
+    arr = convert_bgr_array_to_rgb_array(np.asarray(img))
+    arr = arr.reshape([224*224*3]) if (resize) else arr.reshape([img_data.width*img_data.height*3])
+    return arr
 
-# def base_filter(img_data):
-#     mode = 'RGBA' if (img_data.nChannels == 4) else 'RGB' 
-#     img = Image.frombytes(mode=mode, data=img_data.data, size=[img_data.width, img_data.height])
-#     img = img.convert('RGB') if (mode == 'RGBA') else img
-#     # turn the first pixel's rgb values to 1
-#     if(img.getpixel((0, 0))[0] > 100):
-#         return True
-#     else:
-#         return False
+def base_filter(img_data):
+    mode = 'RGBA' if (img_data.nChannels == 4) else 'RGB' 
+    img = Image.frombytes(mode=mode, data=img_data.data, size=[img_data.width, img_data.height])
+    img = img.convert('RGB') if (mode == 'RGBA') else img
+    # turn the first pixel's rgb values to 1
+    if(img.getpixel((0, 0))[0] > 100):
+        return True
+    else:
+        return False
     
 
-# image_df = ImageBatch("spark://TotallyNormalPC:7077", file_dir)
-# print(len(image_df))
-# resized_df = image_df.filter(base_filter, args=())
+image_df = ImageBatch("spark://TotallyNormalPC:7077", file_dir)
+resize_df = image_df.map(resize_img, (True,), "data_as_array")
+filtered_df = resize_df.filter(base_filter, args=())
 # resized_df.printSchema()
-# resized_df.show(20, truncate=True)
-# print(len(resized_df))
-
-# another_resized_df = resized_df.map(resize_img, (False,), "data_as_array")
+# filtered_df.show(truncate=True)
+filtered_df.save('Answer')
+# another_resized_df = 
 # another_resized_df.printSchema()
 # another_resized_df.show(20, truncate=True)
